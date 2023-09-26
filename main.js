ac948f6adeac84441932bbc049aaa73df654446f
Gabriel04092007 <128478236+Gabriel04092007@users.noreply.github.com>
2023-09-26 08:49:57 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
-document.querySelector('.tecla_pow');
+function tocaSomPom(){
+    document.querySelector('#som_tecla_pom').play()
+}
 
-document.querySelector('#som_tecla_pom').play()
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
