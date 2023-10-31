7c6c1df268d798aff10f6740ac79b03b5cbd0517
Gabriel04092007 <128478236+Gabriel04092007@users.noreply.github.com>
2023-10-31 09:01:24 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play()
+function tocaSomPom(IdElementoAudio){
+    document.querySelector(IdElementoAudio).play()
 }
 
+const ListaDeTeclas = document.querySelectorAll('.tecla')
+
+ListaDeTeclas[0].onclick = TocaSomPom
+
+let contador = 0
+
+while(contador < ListaDeTelcas.lenght){
+    ListaDeTeclas[contador].onclick = function (){
+        TocaSom('#som_tecla_pom')
+    }
+    contador = contador + 1
+    console.log(contador)
+}
